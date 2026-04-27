0850f831789ddc92856653a9eeaf7c6f78029aff
Nancy (via Fern) <nancy@jamello.com>
2026-04-27 09:16:52 -0700

    Adjust published works background color

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const BLUE = "#94a7ab";
 const WINE = "rgb(192, 85, 118)";
 const BLUE_DARK = "#7a9195";
 const ABOUT_BG = WINE;
-const SECTION_ROSE = "#55ACC0";
+const SECTION_ROSE = "#7ba9b5";
 
 const DVDS = [
   {
